95adf300ac2c87f0f43d10cef7bfdebd8e8a44f3
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-24 10:36:03 +0100

    refactor

--- src/main/scala/Sublist.scala
+++ src/main/scala/Sublist.scala
@@ -2,7 +2,7 @@ enum Sublist:
    case Equal, Unequal, Superlist, Sublist
 
 object Sublist:
-   def sublist(l1: List[Int], l2: List[Int]): Sublist =
+   def sublist[T](l1: List[T], l2: List[T]): Sublist =
       (l1, l2) match
          case (Nil, Nil) | _ if l1 == l2 => Equal
          case _ if l1.containsSlice(l2)  => Superlist
