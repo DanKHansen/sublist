386c8d65013e8151e960513f9a50ce7130b508de
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-24 10:33:16 +0100

    refactor

--- src/main/scala/Sublist.scala
+++ src/main/scala/Sublist.scala
@@ -3,8 +3,8 @@ enum Sublist:
 
 object Sublist:
    def sublist(l1: List[Int], l2: List[Int]): Sublist =
-      (l1.mkString("|"), l2.mkString("|")) match
-         case ("", "") | _ if l1 == l2 => Equal
-         case t if t._1.contains(t._2) => Superlist
-         case t if t._2.contains(t._1) => Sublist
-         case _                        => Unequal
+      (l1, l2) match
+         case (Nil, Nil) | _ if l1 == l2 => Equal
+         case _ if l1.containsSlice(l2)  => Superlist
+         case _ if l2.containsSlice(l1)  => Sublist
+         case _                          => Unequal
