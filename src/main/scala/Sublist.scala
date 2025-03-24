2325e451fb9f3f7876999c08fa12df29219323ec
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-24 10:31:43 +0100

    solution

--- src/main/scala/Sublist.scala
+++ src/main/scala/Sublist.scala
@@ -3,6 +3,8 @@ enum Sublist:
 
 object Sublist:
    def sublist(l1: List[Int], l2: List[Int]): Sublist =
-      (l1, l2) match
+      (l1.mkString("|"), l2.mkString("|")) match
+         case ("", "") | _ if l1 == l2 => Equal
          case t if t._1.contains(t._2) => Superlist
-         case t if  t._1 == t._2 => Equal
+         case t if t._2.contains(t._1) => Sublist
+         case _                        => Unequal
